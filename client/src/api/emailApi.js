7e7c93c5aff11d2fdaadbf8edaca6dbd437cd6fd
tiwaririjul <tiwaririjul7@gmail.com>
2024-12-10 19:24:57 +0530

    client

--- client/src/api/emailApi.js
+++ client/src/api/emailApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/emails";
+const API_URL = "https://email-scheduling-server-1.onrender.com/api/emails";
 
 export const sendEmail = async (email) => {
   try {
--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 require("./cron/emailJob");
 
+const PORT = process.env.PORT || 4000;
+
 const emailRoutes = require("./routes/emailRoutes");
 
 const app = express();
@@ -11,5 +13,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/emails", emailRoutes);
+app.get("/working", (req, res) => {
+  res.json({ message: "server is working" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
 module.exports = app;
